b878175fcf1a5d9e116d54e91e97ef4e33832937
AnasElmaliki69 <151750800+AnasElmaliki69@users.noreply.github.com>
2024-03-11 14:29:00 +0100

    tout les fichier ajouté

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+import { divide as test, add, substract } from "./math";
+
+const divide = () => "Fake";
+const a = 45;
+const b = 42;
+
+console.log(divide(a, b));
---
+++ math.js
@@ -0,0 +1,12 @@
+export function divide(a, b) {
+    if (b === 0){
+        return "attemted to devide the numbers";
+    }
+    return a / b;
+}
+
+export const add = (a, b) => a + b;
+export const substract = (a, b) => a - b;
+export const multiply = (a, b) => a / b;
+
+
